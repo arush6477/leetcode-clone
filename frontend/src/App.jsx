57b16a39daaba29cbd168aa3b2149ab5245eb446
Arush Choudhary <arushgzbin@gmail.com>
2025-08-05 17:03:05 +0530

    making ui hating life git add .git add .!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react"
+import LoginPage from "./page/LoginPage"
+import SignUpPage from "./page/SignUpPage"
+import HomePage from "./page/HomePage"
+import { Route, 
+  Routes, 
+  Router,
+  Navigate 
+} from "react-router-dom"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  let authUser = null
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="flex flex-col items-center justify-start">
+      <Routes>
+        <Route path="/" element={authUser? <HomePage /> : <Navigate to={"/login"}/>}/>
 
-export default App
+        <Route path="/login" element={!authUser? <LoginPage /> : <Navigate to={"/"}/>} />
+
+        <Route path="/signup" element={!authUser? <SignUpPage/>: <Navigate to={"/"}/>} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/libs/axios.js
@@ -0,0 +1,10 @@
+import axios from "axios"
+
+const axiosInstance = axios.create({
+    baseURL:import.meta.env.MODE === "development" ? "http://localhost:8081/api/v1" : "/api/v1",
+    withCredentials:true
+})
+
+export {
+    axiosInstance
+}
